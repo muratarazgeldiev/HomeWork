d7122a38305d77acf68a4cc0dc9c281758e3d0e9
muratarazgeldiev <s901@tmm-sapr.org>
2013-11-03 23:28:48 +0200

    HomeWokr

---
+++ src/main/java/Lecture5/ExampleSet.java
@@ -0,0 +1,11 @@
+package Lecture5;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: admin
+ * Date: 03.11.13
+ * Time: 22:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExampleSet {
+}
